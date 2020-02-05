f8d27f8d10f6c85a3674d74811a4fdbc87b2b9b2
draro <58738792+draro@users.noreply.github.com>
2020-02-05 08:52:07 -0500

    heroku db

--- server.js
+++ server.js
@@ -10,11 +10,8 @@ const image = require('./controllers/image')
 const db = knex({
     client: 'pg',
     connection: {
-        host: 'postgresql-solid-35436',
-        port: '5432',
-        user: 'postgres',
-        password: '',
-        database: 'smart-brain'
+        connectionString: process.env.DATABASE_URL,
+        ssl: true,
     }
 });
 
